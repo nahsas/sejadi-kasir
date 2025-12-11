8acecfa5eeabdb3783ac51d8f5c3fee88fd294cd
Nataa <ardinata2408@gmail.com>
2025-12-11 08:56:56 +0000

    Ganti background nya menjadi putih (_for element <Input>_)

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -288,7 +288,7 @@ export default function MenuPage() {
                   <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
                   <Input
                     placeholder="Cari nama item..."
-                    className="pl-10"
+                    className="pl-10 bg-white"
                     value={stockSearchTerm}
                     onChange={(e) => setStockSearchTerm(e.target.value)}
                   />
