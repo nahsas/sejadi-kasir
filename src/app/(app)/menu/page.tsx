cf16e3c9776ee526f9d190f6b3fc106763e6cdd6
Nataa <ardinata2408@gmail.com>
2025-12-11 08:58:10 +0000

    Ganti background nya menjadi putih (_for element <Primitive.button>_)

--- src/app/(app)/menu/page.tsx
+++ src/app/(app)/menu/page.tsx
@@ -294,7 +294,7 @@ export default function MenuPage() {
                   />
                 </div>
                 <Select value={stockFilterAvailability} onValueChange={setStockFilterAvailability}>
-                  <SelectTrigger className="w-full md:w-[240px]">
+                  <SelectTrigger className="w-full md:w-[240px] bg-white">
                     <div className="flex items-center gap-2">
                       <Filter className="h-4 w-4" />
                       <SelectValue placeholder="Filter Ketersediaan" />
